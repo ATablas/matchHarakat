bfe171be963d0e49e4296d29ccf4ac65888eb20e
AT <32175792+ATablas@users.noreply.github.com>
2020-05-11 05:08:12 +0300

    Update harakat_lib.php

--- harakat_lib.php
+++ harakat_lib.php
@@ -13,18 +13,18 @@ function str_split_unicode($str, $l = 0) {
     return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
 }
 
-function harkatMatching($src, $elWithHarakat)
+function harkatMatching($src, $strWithHarakat)
 {
   $matching = false;
   $harakatArr = array('َ','ً','ُ','ٌ','ِ','ٍ','ْ','ّ');
 
   $src_Chars = str_split_unicode($src);
 
-  $elWithHarakat_Chars = str_split_unicode($elWithHarakat);
-  for($n=0,$i=0; $n<count($elWithHarakat_Chars); $n++)
+  $strWithHarakat_Chars = str_split_unicode($strWithHarakat);
+  for($n=0,$i=0; $n<count($strWithHarakat_Chars); $n++)
   {
     //check if the current char is a harakah, then compare.
-    if($i >= count($src_Chars) && !in_array($elWithHarakat_Chars[$n],$harakatArr)) //it means even with advancing the $n, there's at least 1 letter (not harakah) more there than here.
+    if($i >= count($src_Chars) && !in_array($strWithHarakat_Chars[$n],$harakatArr)) //it means even with advancing the $n, there's at least 1 letter (not harakah) more there than here.
     {
       $matching = false;
       break;
@@ -36,10 +36,10 @@ function harkatMatching($src, $elWithHarakat)
     if(in_array($src_Chars[$i], $harakatArr))
     {
       //check if the current char in element is harakah too, they must be the same then.
-      if(in_array($elWithHarakat_Chars[$n], $harakatArr))
+      if(in_array($strWithHarakat_Chars[$n], $harakatArr))
       {
         //are they the same?
-        if($elWithHarakat_Chars[$n]==$src_Chars[$i])
+        if($strWithHarakat_Chars[$n]==$src_Chars[$i])
         {
           $matching = true;
           $i++;
@@ -57,12 +57,12 @@ function harkatMatching($src, $elWithHarakat)
     }
     else //this is not a harakah in the src
     {
-      if(in_Array($elWithHarakat_Chars[$n], $harakatArr))
+      if(in_Array($strWithHarakat_Chars[$n], $harakatArr))
       {
         $n++; //advance one char (to the next letter)
-        if(in_array($elWithHarakat_Chars[$n], $harakatArr)) $n++; //sometimes 2 harakat are consecutive (shaddah + harakah)
+        if(in_array($strWithHarakat_Chars[$n], $harakatArr)) $n++; //sometimes 2 harakat are consecutive (shaddah + harakah)
 
-        if($elWithHarakat_Chars[$n]==$src_Chars[$i])  //compare letters now
+        if($strWithHarakat_Chars[$n]==$src_Chars[$i])  //compare letters now
         {
           $matching = true;
           $i++;
@@ -75,7 +75,7 @@ function harkatMatching($src, $elWithHarakat)
       }
       else //also this el char is not a harakah
       {
-        if($elWithHarakat_Chars[$n]==$src_Chars[$i])
+        if($strWithHarakat_Chars[$n]==$src_Chars[$i])
         {
           $matching = true;
           $i++;
